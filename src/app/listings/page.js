19262860cf65eb577f66f805b990c61117bfeb2e
John Alexis Guerra Gómez <john.guerra@gmail.com>
2025-10-21 16:04:08 -0700

    Adding basic next application

---
+++ src/app/listings/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Listings() {
+  return (
+    <div className="font-sans grid">
+      <main className="p-6" >
+        <h1>
+          Listings
+        </h1>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+
+      </footer>
+    </div>
+  );
+}
